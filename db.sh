ae31391c326197f2bfb68cdeb00fa461002c88ab
root <root@ip-172-31-16-127.us-east-2.compute.internal>
2024-01-01 11:00:48 +0000

    fsdfs

--- db.sh
+++ db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mysql -e "CREATE DATABASE ecomdb;CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';FLUSH PRIVILEGES;"
-mysql < /home/ec2-user/ansible_playbook_ecmorce/mariaDB/tasks/db-load-script.sql
+mysql < /home/ansible/ansible_playbook_ecmorce/db-load-script.sql
